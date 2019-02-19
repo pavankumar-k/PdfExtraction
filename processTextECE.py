fe87d64e9849381362e85a9aa6cc542aea2bc69f
pavankumar-k <32760280+pavankumar-k@users.noreply.github.com>
2019-02-19 21:19:40 +0530

    Add files via upload

--- processTextECE.py
+++ processTextECE.py
@@ -4,12 +4,12 @@ Created on Thu Jan 17 13:17:08 2019
 Pavan
 """
 import re
-import xlsxwriter
+#import xlsxwriter
 
 
 #Author affiliation matcher
 def authaffli(auth,affli):
-    authlis = re.split(r'(, )|( & )|( and )')
+    authlis = re.split(r'(, )|( & )|( and )',auth)
     aflis = affli.split(';')
     autflis = []
     for a in authlis:
@@ -25,16 +25,19 @@ def authaffli(auth,affli):
 #Author line identifier
 def checkAuthorline(line):
     # replace all special characters other than letters,space and ,
-    line = (re.sub(r'[a-zA-z/s]','',line)).replace(' and ',' ')
+    #print('GIVENAUTHORLINE',line)
+    line = (re.sub(r'^[a-zA-Z/s]','',line)).replace(' and ',' ').strip()
     # if only one word exist not an author
     if len(line.split(' '))<2:return False
     count = 0
+    #print(line.split(' '))
     #split with (, ) and check for the camel case in each word
-    for a in line.split(', '):
-        if re.match(r'^[A-Z]]') is not None:
+    for a in line.split(' '):
+        if re.match(r'[A-Z]',a.strip()[0]) is not None:
             count+=1
+    #print(count)
     #if most of the letters have camel case it is author line else not an author line
-    if count == len(line.split(', ')) or count== len(line.split(', '))-1:return True
+    if count == len(line.split(' ')) or count== len(line.split(' '))-1:return True
     else: return False
 
 # header processor
@@ -45,7 +48,7 @@ def getheadfields(headlines):
         title = ''
         #if there are threee lines use as title, auth and affli
         title = headlines[0].strip()
-        auth = headlines[1].strip().split()
+        auth = headlines[1].strip()
         affli = headlines[2].strip()
         for a in authaffli(auth,affli):
             det.append({'author':a[0],'title':title,'affli':a[1]})
@@ -53,12 +56,15 @@ def getheadfields(headlines):
         title = ''
         authaflis = []
         authdet = ''
-        for line in headlines:
-            if title!='' and checkAuthorline == True:
-                authdet+=line+'\n'
-            else:
-                title+=line.strip()
+        n=0
+        while n<len(headlines):
+            if checkAuthorline(headlines[n]) == True:break
+            n+=1
+        title = ' '.join([x.strip() for x in headlines[0:n]])
+        authdet = '\n'.join([x.strip() for x in headlines[n:]])
+        print('Title', title)
         if authdet == '': raise Exception('author details is not found:\n'+title)
+
         # auth det contains numbers check from last line and if 1 occurs at start of the line - lines above are authors
         if re.match(r'[0-9]+',authdet.split('\n')[0]) is not None:
             lines = authdet.split('\n')
@@ -67,6 +73,7 @@ def getheadfields(headlines):
                 if lines[i].match(r'^1[A-z]') is not None:break
                 i-=1
             authaflis = authaffli(' '.join(lines[0:i]),' '.join(lines[i:]))
+            print('Inside num type')
         elif re.match(r', ',authdet.split('\n')[0]) is not None:
             lines = authdet.split('\n')
             i = 0
@@ -74,6 +81,7 @@ def getheadfields(headlines):
                 if lines[i].match(r'( & )|( and )') is not None:break
                 i+=1
             authaflis = authaffli(' '.join(lines[0:i+1]),' '.join(lines[i+1:]))
+            print('Inside commas')
         else:
             lines = authdet.split('\n')
             authaflis = [[lines[0].strip(),' '.join(lines[1:])]]
@@ -92,7 +100,7 @@ file = 'ECE2018.txt'
 import re
 #to match just before the ABS number
 ANo = re.compile('^(?=[A-Z]{1,4}[\d]+[\.]{0,1}[\d]{0,1}$)')
-AbsEndLine = re.compile('^DOI: 10[\.]1530/[\w\.]+$')
+AbsEndLine = re.compile('\nDOI: 10[\.]1530/[\w\.]+\n')
 
 records = []
 output = ''
@@ -100,43 +108,47 @@ with open(file, encoding='utf-') as file:
     for line in file.readlines()[:]:
         output+=line
 #split just before abs number
-records = re.split(r'^(?=[A-Z]{1,4}[\d]+[\.]{0,1}[\d]{0,1}$)',output)
+records = re.split(r'[\n]+[A-Z]{1,4}[\d]+[\.]{0,1}[\d]{0,1}[\n]+',output)
+#print(output.split('\n')[0])
 print(len(records))
-print(records[1])
+#print(records[1])
 input('Test for the ANO:')
 rows = []
 for item in records[1:]:
     try:
+        print(item)
         #Affiliations are ended by .\n
-        lines = item.split('.\n')
+        lines = re.split('\.[\n]+',item)
         head = lines[0]
         body = '.\n'.join(lines[1:])
         text = ''
         doi = ''
         # processing body of abstract by cutting upto DOI
-        for line in body.split('\n'):
-            if line.match(r'^DOI: 10[\.]1530/[\w\.]+$') is not None:
+        for line in re.split('\n+',body):
+            if re.match(AbsEndLine,line) is not None:
                 doi = line
                 break
             else:
                 text+=line.strip()
-        print('DOI',doi)
-        print('TEXT:\n',text)
-        input('TEST TEXT VERIFIER')
+        #print('DOI',doi)
+        #print('TEXT:\n',text)
+        #input('TEST TEXT VERIFIER')
         # get title author and affiliation fields(as list of dicts)
-        headlines = head.split('\n')
-        absnum = headlines[0].strip()
-        for a in getheadfields(headlines[1:]):
-            row=[a['author'],a['affli'],a['title'],absnum,text,doi]
+        headlines = re.split('\n+',head)
+        print(head)
+        #input('Test Lines')
+        for a in getheadfields(headlines[0:]):
+            row=[a['author'],a['affli'],a['title'],text,doi]
             print(row)
             input('Test for each author and affli:')
             rows.append(row)
     except Exception as e:
         print('UNABLE TO PARSE THE TEXT :', e)
-        with open('error.txt','a') as file:
-            file.write(item)
-            file.close()
-
+    #    with open('error.txt','a',encoding='utf-8') as file:
+    #        file.write(item)
+    #        file.close()
+    input('NEXT ITEM')
+'''
 ############################### Excel Writer #####################################
 FOutput = 'ECE2018.xlsx'
 workbook = xlsxwriter.Workbook(FOutput, {'constant_memory': True})
@@ -153,3 +165,4 @@ for row in rows:
         cl = cl + 1
     rw += 1
 workbook.close()
+'''
